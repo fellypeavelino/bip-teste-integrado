32442b8727ff70e0bf6616fa4eff78dee96870ef
fellypeavelino <fellypeavelino@hotmail.com>
2026-02-16 19:14:05 -0300

    configurando para criar as tabelas

---
+++ ejb-module/beneficio-ejb-rest/src/main/resources/META-INF/load.sql
@@ -0,0 +1,2 @@
+INSERT INTO Beneficio (id, descricao, valor, version) VALUES (1, 'Vale Alimentação', 1000.00, 1);
+INSERT INTO Beneficio (id, descricao, valor, version) VALUES (2, 'Vale Refeição', 500.00, 1);
